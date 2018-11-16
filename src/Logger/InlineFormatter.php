c08d58d1bb2c8e3eea29f44127e1b6a393ae317b
Mark White <mark@somsip.com>
2018-11-16 14:20:27 +0700

    Added InlineFormatter back in

---
+++ src/Logger/InlineFormatter.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace Somsip\BlcKnd\Logging;
+
+use Illuminate\Log\Logger;
+
+use Monolog\Processor\IntrospectionProcessor;
+use Monolog\Logger as Monolog;
+
+use Somsip\BlcKnd\Logger\Formatter\CallerInlineFormatter;
+
+class InlineFormatter
+{
+    /**
+     * Customize the given logger instance.
+     *
+     * @param  \Illuminate\Log\Logger  $logger
+     * @return void
+     */
+    public function __invoke(Logger $logger)
+    {
+        // FIXME: This needs to happen somewhere else, like a custom logger
+        $ignores = [
+            'Facade',
+            'Logger',
+            'LogManager'
+        ];
+        $logger->pushProcessor(new IntrospectionProcessor(Monolog::DEBUG, $ignores));
+
+        foreach ($logger->getHandlers() as $handler) {
+            $handler->setFormatter(new CallerInlineFormatter());
+        }
+    }
+}
