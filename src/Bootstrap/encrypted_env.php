f04ed4d1ed8f33b44e5ca3de4715d6551210b808
Mark White <mark@somsip.com>
2019-03-06 17:13:36 +0700

    Retrieve encrypted envs from $_ENV

--- src/Bootstrap/encrypted_env.php
+++ src/Bootstrap/encrypted_env.php
@@ -14,7 +14,7 @@ if (!function_exists('encrypted_env')) {
      */
     function encrypted_env(string $key, $default = null)
     {
-        $value = getenv($key);
+        $value = get_env($key);
 
         if ($value === false) {
             return value($default);
@@ -59,7 +59,7 @@ if (!function_exists('decrypt_env')) {
     function decrypt_env(string $encoded): string
     {
         // @codeCoverageIgnoreStart
-        if (!getenv('APP_KEY')) {
+        if (!get_env('APP_KEY')) {
             throw new RuntimeException('APP_KEY must be set in environmental variables');
         }
         // @codeCoverageIgnoreEnd
@@ -68,7 +68,7 @@ if (!function_exists('decrypt_env')) {
         $method = 'AES-256-CBC';
         $ivSize = openssl_cipher_iv_length($method);
         $iv = substr($data, 0, $ivSize);
-        $key = openssl_digest(getenv('APP_KEY'), 'sha256');
+        $key = openssl_digest(get_env('APP_KEY'), 'sha256');
         $data = openssl_decrypt(substr($data, $ivSize), $method, $key, OPENSSL_RAW_DATA, $iv);
         return $data;
     }
@@ -84,7 +84,7 @@ if (!function_exists('encrypt_env')) {
     function encrypt_env(string $text): string
     {
         // @codeCoverageIgnoreStart
-        if (!getenv('APP_KEY')) {
+        if (!get_env('APP_KEY')) {
             throw new RuntimeException('APP_KEY must be set in environmental variables');
         }
         // @codeCoverageIgnoreEnd
@@ -92,11 +92,27 @@ if (!function_exists('encrypt_env')) {
         $method = 'AES-256-CBC';
         $ivSize = openssl_cipher_iv_length($method);
         $iv = openssl_random_pseudo_bytes($ivSize);
-        $key = openssl_digest(getenv('APP_KEY'), 'sha256');
+        $key = openssl_digest(get_env('APP_KEY'), 'sha256');
         $encrypted = openssl_encrypt($text, $method, $key, OPENSSL_RAW_DATA, $iv);
 
         // For storage/transmission, we simply concatenate the IV and cipher text
         $encrypted = base64_encode($iv . $encrypted);
         return "ENC:$encrypted";
+    }
+}
+if (!function_exists('get_env')) {
+    /**
+     * Retrieves the env from $_ENV now Laravel overwrites getenv()
+     *
+     * @param string $text
+     * @return mixed
+     * TODO: Ambiguous return value cannot be specified in PHP7.0
+     */
+    function get_env(string $text)
+    {
+        if (!empty($_ENV[$text])) {
+            return $_ENV[$text];
+        }
+        return false;
     }
 }
