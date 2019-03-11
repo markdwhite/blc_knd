fd643b3c3c04a62cbcbcced9528ac384c68eaf26
Mark White <mark@somsip.com>
2019-03-11 10:55:38 +0700

    Try using laravel env() to see if these will work when cached in immutable env

--- src/Bootstrap/encrypted_env.php
+++ src/Bootstrap/encrypted_env.php
@@ -14,25 +14,10 @@ if (!function_exists('encrypted_env')) {
      */
     function encrypted_env(string $key, $default = null)
     {
-        $value = get_env($key);
+        $value = env($key, false);
 
         if ($value === false) {
             return value($default);
-        }
-
-        // Values can look like '(true)'
-        switch (strtolower(trim($value, '()'))) {
-            case 'true':
-                return true;
-
-            case 'false':
-                return false;
-
-            case 'empty':
-                return '';
-
-            case 'null':
-                return;
         }
 
         if (strpos($value, '"') === 0 && substr($value, -1) === '"') {
@@ -59,7 +44,7 @@ if (!function_exists('decrypt_env')) {
     function decrypt_env(string $encoded): string
     {
         // @codeCoverageIgnoreStart
-        if (!get_env('APP_KEY')) {
+        if (!env('APP_KEY')) {
             throw new RuntimeException('APP_KEY must be set in environmental variables');
         }
         // @codeCoverageIgnoreEnd
@@ -68,7 +53,7 @@ if (!function_exists('decrypt_env')) {
         $method = 'AES-256-CBC';
         $ivSize = openssl_cipher_iv_length($method);
         $iv = substr($data, 0, $ivSize);
-        $key = openssl_digest(get_env('APP_KEY'), 'sha256');
+        $key = openssl_digest(env('APP_KEY'), 'sha256');
         $data = openssl_decrypt(substr($data, $ivSize), $method, $key, OPENSSL_RAW_DATA, $iv);
         return $data;
     }
@@ -84,7 +69,7 @@ if (!function_exists('encrypt_env')) {
     function encrypt_env(string $text): string
     {
         // @codeCoverageIgnoreStart
-        if (!get_env('APP_KEY')) {
+        if (!env('APP_KEY')) {
             throw new RuntimeException('APP_KEY must be set in environmental variables');
         }
         // @codeCoverageIgnoreEnd
@@ -92,27 +77,11 @@ if (!function_exists('encrypt_env')) {
         $method = 'AES-256-CBC';
         $ivSize = openssl_cipher_iv_length($method);
         $iv = openssl_random_pseudo_bytes($ivSize);
-        $key = openssl_digest(get_env('APP_KEY'), 'sha256');
+        $key = openssl_digest(env('APP_KEY'), 'sha256');
         $encrypted = openssl_encrypt($text, $method, $key, OPENSSL_RAW_DATA, $iv);
 
         // For storage/transmission, we simply concatenate the IV and cipher text
         $encrypted = base64_encode($iv . $encrypted);
         return "ENC:$encrypted";
-    }
-}
-if (!function_exists('get_env')) {
-    /**
-     * Retrieves the env from $_ENV now Laravel overwrites getenv()
-     *
-     * @param string $text
-     * @return mixed
-     * TODO: Ambiguous return value cannot be specified in PHP7.0
-     */
-    function get_env(string $text)
-    {
-        if (!empty($_ENV[$text])) {
-            return $_ENV[$text];
-        }
-        return false;
     }
 }
